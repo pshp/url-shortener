a12698e2300da793e70e4a499b77ca964b757e3a
pshp <phirschparker@gmail.com>
2025-07-28 22:47:17 +0200

    refactor(model): reduce short_url column to 8 chars

--- src/main/kotlin/io/github/pshp/shortener/model/UrlMappingModel.kt
+++ src/main/kotlin/io/github/pshp/shortener/model/UrlMappingModel.kt
@@ -25,10 +25,10 @@ data class UrlMappingModel(
     // fullHash is in internal field not visible to the api consumer
     // Stored in case we want to change the shortCode length in the future
     @JsonIgnore
-    @Column(name = "full_hash", nullable = false, unique = true)
+    @Column(name = "full_hash", nullable = false)
     val fullHash: String,
 
-    @Column(name = "short_code", nullable = false, unique = true)
+    @Column(name = "short_code", nullable = false, unique = true, length = 8)
     val shortCode: String,
 
     @CreationTimestamp
